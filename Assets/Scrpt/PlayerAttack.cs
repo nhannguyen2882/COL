8e9d63ee7b8aa6fff4b1a1b6efde4152521daadf
Cheer-313 <nhh3103@gmail.com>
2020-11-29 17:25:00 +0700

    Wall, HealthBar, ...

--- Assets/Scrpt/PlayerAttack.cs
+++ Assets/Scrpt/PlayerAttack.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-
+    public Transform attackPoint;
+    public float attackRange = 0.2f;
+    public LayerMask enemyLayers;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Attack();
+        }
     }
 
     void Attack()
     {
+        Collider2D[] hitEnemis = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+    }
 
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
