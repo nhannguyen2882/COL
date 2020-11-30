325a6402c36a812794b9312dfe2a7a92ad113358
Cheer-313 <nhh3103@gmail.com>
2020-11-30 15:14:18 +0700

    committ t2

---
+++ Assets/Scrpt/Creep.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creep : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    int currentHealt;
+
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealt = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealt -= damage;
+        animator.SetTrigger("Hit");
+        healthBar.SetHealth(currentHealt);
+
+        if(currentHealt <= 0)
+        {
+            healthBar.SetHealth(0);
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+        animator.SetBool("isDead",true);
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
--- Assets/Scrpt/PlayerAttack.cs
+++ Assets/Scrpt/PlayerAttack.cs
@@ -7,18 +7,31 @@ public class PlayerAttack : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.2f;
     public LayerMask enemyLayers;
+
+    public int playerDamage;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if(Time.time >= nextAttackTime)
         {
-            Attack();
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
         }
+        
     }
 
     void Attack()
     {
         Collider2D[] hitEnemis = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemis)
+        {
+            enemy.GetComponent<Creep>().TakeDamage(playerDamage);
+        }
     }
 
     void OnDrawGizmosSelected()
