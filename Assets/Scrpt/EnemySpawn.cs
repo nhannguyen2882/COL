34debf7e3174fb932c51b9ba8d78879f82e73112
Nhan Nguyen <baddreamlove2882@gmail.com>
2020-12-10 03:31:44 +0700

    n

--- Assets/Scrpt/EnemySpawn.cs
+++ Assets/Scrpt/EnemySpawn.cs
@@ -7,7 +7,7 @@ public class EnemySpawn : MonoBehaviour
 {
 	public GameObject[] spawnEnemy;
 	public int spawnRate = 5;
-	public int startX = -180, endX = 10;
+	public int level = 0;
 	public int startY = 0, endY = 7;
 	bool isSpawn = false;
 	GameObject enemy;
@@ -40,31 +40,59 @@ public class EnemySpawn : MonoBehaviour
 		 if(collider.gameObject.name== "Player" && !isSpawn)
 		 {
 			Debug.Log("Trigger");
-			
-            System.Random random = new System.Random();
-			int indexEnemy;
-			int x, y;
-			Vector3 position;
-			for (int i =0;i<spawnRate;i++)
-            {
-				indexEnemy = Random.Range(0, spawnEnemy.Length);
-				x = Random.Range(startX, endX);
-				y = Random.Range(startY, endY);
-				position = new Vector3(x, y, 0);
-				while (availablePlaces.Contains(position))
-				{
-					x = Random.Range(startX, endX);
-					y = Random.Range(startY, endY);
-					position = new Vector3(x, y, 0);
-				}
-				Debug.Log(position.ToString());
-				Instantiate(spawnEnemy[indexEnemy], new Vector2(position.x,position.y)
-						 ,
-						 Quaternion.identity);
+			int startX,endX;
+			if(level == 0){
+				startX = -200;
+				endX = -183;
+				Spawn(startX,endX,spawnRate/3);
+				startX = -155;
+				endX = -124;
+				Spawn(startX,endX,spawnRate/3);
+				startX = -118;
+				endX = -95;
+				Spawn(startX,endX,spawnRate/3);
+			}else if(level == 1){
+				startX = -85;
+				endX = -56;
+				Spawn(startX,endX,spawnRate);
+			}else if(level == 2){
+				startX = -50;
+				endX = 30;
+				Spawn(startX,endX,spawnRate);
+			}else{
+				startX = 40;
+				endX = 23;
+				Spawn(startX,endX,spawnRate);
 			}
+
 			isSpawn = true;
 		 }
 		 //spawnenemy.LookAtTarget=gameObject;
 		 //Debug.Log("SPAWN ENEMIES!");
+	}
+	void Spawn(int startX,int endX, int rate){
+		System.Random random = new System.Random();
+		int indexEnemy;
+		int x, y;
+		Vector3 position,p1,p2;
+		for (int i =0;i<rate;i++)
+		{
+			indexEnemy = Random.Range(0, spawnEnemy.Length);
+			x = Random.Range(startX, endX);
+			y = Random.Range(startY, endY);
+			position = new Vector3(x, y, 0);
+			p1 = new Vector3(x, y - 2, 0);
+			p2 = new Vector3(x, y + 2, 0);
+			while (availablePlaces.Contains(position) || availablePlaces.Contains(p1) || availablePlaces.Contains(p2))
+			{
+				x = Random.Range(startX, endX);
+				y = Random.Range(startY, endY);
+				position = new Vector3(x, y, 0);
+				p1 = new Vector3(x, y - 2, 0);
+				p2 = new Vector3(x, y + 2, 0);
+			}
+			Debug.Log(position.ToString());
+			Instantiate(spawnEnemy[indexEnemy], new Vector2(position.x,position.y),Quaternion.identity);
+		}
 	}
 }
